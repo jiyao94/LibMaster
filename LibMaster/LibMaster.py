6d83f5c59fc06882b3006a2f669d4112c95fcee4
jiyao94 <jiyao94@hotmail.com>
2018-07-23 10:37:09 +0800

    update comments

--- LibMaster/LibMaster.py
+++ LibMaster/LibMaster.py
@@ -1,3 +1,12 @@
+###############################################################################
+'''
+AUTHOR:			Yao Ji (jiyao94@126.com)
+CREATED DATE:	2018/7/23
+LAST UPDATE:	2018/7/23
+DESCRIPTION:	Main program to start UI.
+'''
+###############################################################################
 import pyforms
 from UI import LibMaster
+
 pyforms.start_app(LibMaster)
